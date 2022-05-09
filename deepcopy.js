8a80460b58faf328e51fdb5a9c785d23a92e40c4
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-05-09 12:43:42 +0900

    spell error check

--- deepcopy.js
+++ deepcopy.js
@@ -1,6 +1,6 @@
 let num1 = 1;
 let num2 = num1;
 
-console.log("Initial :", num1, num2); // Initial: 1 1
+console.log("Initial:", num1, num2); // Initial: 1 1
 num1 = 3;
-console.log("Exchange:", num1, num2); // Changed: 3 1
+console.log("Changed:", num1, num2); // Changed: 3 1
