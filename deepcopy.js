43350e5b8d751cb67b3cdba54fdfc83501577778
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-05-09 12:39:13 +0900

    added tests

---
+++ deepcopy.js
@@ -0,0 +1,6 @@
+let num1 = 1;
+let num2 = num1;
+
+console.log("Initial :", num1, " ", num2);
+num1 = 3;
+console.log("Exchange:", num1, " ", num2);
---
+++ shallowcopy.js
@@ -0,0 +1,8 @@
+const obj1 = {
+  value: "initial",
+};
+const obj2 = obj1;
+const obj3 = JSON.parse(JSON.stringify(obj1));
+console.log("Initial:", obj1.value, obj2.value, obj3.value);
+obj2.value = "newvalue";
+console.log("Changed:", obj1.value, obj2.value, obj3.value);
