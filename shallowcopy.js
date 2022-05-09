b27cb1295062563d28216b991d2cdda5aa01a7de
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-05-09 12:42:18 +0900

    added expected outcomes for dc & sc

--- shallowcopy.js
+++ shallowcopy.js
@@ -3,6 +3,7 @@ const obj1 = {
 };
 const obj2 = obj1;
 const obj3 = JSON.parse(JSON.stringify(obj1));
-console.log("Initial:", obj1.value, obj2.value, obj3.value);
+
+console.log("Initial:", obj1.value, obj2.value, obj3.value); // Initial: initial initial initial
 obj2.value = "newvalue";
-console.log("Changed:", obj1.value, obj2.value, obj3.value);
+console.log("Changed:", obj1.value, obj2.value, obj3.value); // Changed: newvalue newvalue initial
