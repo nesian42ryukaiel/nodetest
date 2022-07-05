555487ac94fe3da40b45ee01be1fe3b298753d06
nesian42ryukaiel <nesianryukaiel@gmail.com>
2022-07-05 19:19:31 +0900

    it seems that Set may have duplicate Objects

--- minesweeper.js
+++ minesweeper.js
@@ -10,8 +10,57 @@ const setBoard = (w, h) => {
   return board;
 };
 
-const b = setBoard(20, 10);
+const plantMines = (board, mines, first) => {
+  const nb = [...board];
+  const by = nb.length,
+    bx = nb[0].length;
+  const minebox = new Set();
+  const dir = [
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+    [0, -1],
+    [-1, -1],
+  ];
+  if (board.length * board[0].length <= mines) {
+    console.log("Invalid mine total!");
+    return;
+  }
+  while (minebox.size < mines) {
+    let mine = [Math.floor(Math.random() * by), Math.floor(Math.random() * bx)];
+    if (mine[0] !== first[0] || mine[1] !== first[1]) {
+      minebox.add(mine);
+    }
+  }
+  console.log(minebox.size, "mines /", mines, "intended");
+  for (let mine of minebox) {
+    console.log(mine);
+    nb[mine[0]][mine[1]] = -9;
+  }
+  for (let i = 0; i < by; i++) {
+    for (let j = 0; j < bx; j++) {
+      if (nb[i][j] < 0) {
+        for (let k = 0; k < 8; k++) {
+          if (
+            i + dir[k][0] >= 0 &&
+            i + dir[k][0] < by &&
+            j + dir[k][1] >= 0 &&
+            j + dir[k][1] < bx
+          ) {
+            if (nb[i + dir[k][0]][j + dir[k][1]] >= 0) nb[i][j]++;
+          }
+        }
+      }
+    }
+  }
+  return nb;
+};
+
+const b = setBoard(7, 7);
+const c = plantMines(b, 7, [3, 3]);
 console.log("----");
 // for (let row of b) console.log(row.length);
-b[3][1] = 3;
-console.log(b);
+console.log(c);
